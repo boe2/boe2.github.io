aa137a91f1a30afcceefaafd2faea717d8e3ce7a
Boehijt02 <boehijt@gmail.com>
2017-01-14 15:12:37 +0100

    Update

---
+++ data/formatinfo.js
@@ -0,0 +1,8 @@
+var formatinfodict = {
+	"competition4101": "Battle of Alola - Junior Division - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/competitions/4101/\"> info about this competition </a>",
+	"competition4102": "Battle of Alola - Masters Division - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/competitions/4102/\"> info about this competition </a>",
+	"oras_117_1": "ORAS Season 17 - Singles - NOTE: displaying ORAS data might cause errors or might have Pokemon missing in the list!",
+	"oras_117_2": "ORAS Season 17 - Doubles - NOTE: displaying ORAS data might cause errors or might have Pokemon missing in the list!",
+	"oras_117_3": "ORAS Season 17 - Triples - NOTE: displaying ORAS data might cause errors or might have Pokemon missing in the list!",
+	"oras_117_4": "ORAS Season 17 - Rotations - NOTE: displaying ORAS data might cause errors or might have Pokemon missing in the list!",
+}
--- scripts/bsus.js
+++ scripts/bsus.js
@@ -4,11 +4,15 @@ var totalusage;
 select = document.getElementById("select");
 var value = select.options[select.selectedIndex].value;
 
-setTotalUsage(value);
+updateFormatInfo(value);
 
 var mon;
 var tab = 0;
 var tabnum = 44;
+
+var tabnames = ["Moves", "Items", "Abilities", "Natures", "Moves when victorious", "Opponents when victorious", "Moves when defeated", "Opponents when defeated", "Teammates"];
+var tabnums = [44, 4, 84, 90, 130, 170, 190, 230, 250];
+
 
 function createButtons() {
 
@@ -155,63 +159,19 @@ function setData(number) {
 
 function setTab(number) {
     tab = number;
+	var buttonname;
 
-    document.getElementById("tabbutton0").className = "coloredbutton";
-    document.getElementById("tabbutton1").className = "coloredbutton";
-    document.getElementById("tabbutton2").className = "coloredbutton";
-    document.getElementById("tabbutton3").className = "coloredbutton";
-    document.getElementById("tabbutton4").className = "coloredbutton";
-    document.getElementById("tabbutton5").className = "coloredbutton";
-    document.getElementById("tabbutton6").className = "coloredbutton";
-    document.getElementById("tabbutton7").className = "coloredbutton";
-    document.getElementById("tabbutton8").className = "coloredbutton";
-
-    if (tab == 0) {
-        tabnum = 44;
-        document.getElementById("tabnamespan").innerHTML = "Moves";
-        document.getElementById("tabbutton0").className = "coloredclickedbutton";
-    }
-    if (tab == 1) {
-        tabnum = 4;
-        document.getElementById("tabnamespan").innerHTML = "Items";
-        document.getElementById("tabbutton1").className = "coloredclickedbutton";
-    }
-    if (tab == 2) {
-        tabnum = 84;
-        document.getElementById("tabnamespan").innerHTML = "Abilities";
-        document.getElementById("tabbutton2").className = "coloredclickedbutton";
-    }
-    if (tab == 3) {
-        tabnum = 90;
-        document.getElementById("tabnamespan").innerHTML = "Natures";
-        document.getElementById("tabbutton3").className = "coloredclickedbutton";
-    }
-    if (tab == 4) {
-        tabnum = 130;
-        document.getElementById("tabnamespan").innerHTML = "Moves when victorious";
-        document.getElementById("tabbutton4").className = "coloredclickedbutton";
-    }
-    if (tab == 5) {
-        tabnum = 170;
-        document.getElementById("tabnamespan").innerHTML = "Opponents when victorious";
-        document.getElementById("tabbutton5").className = "coloredclickedbutton";
-    }
-    if (tab == 6) {
-        tabnum = 190;
-        document.getElementById("tabnamespan").innerHTML = "Moves when defeated";
-        document.getElementById("tabbutton6").className = "coloredclickedbutton";
-    }
-    if (tab == 7) {
-        tabnum = 230;
-        document.getElementById("tabnamespan").innerHTML = "Opponents when defeated";
-        document.getElementById("tabbutton7").className = "coloredclickedbutton";
-    }
-    if (tab == 8) {
-        tabnum = 250;
-        document.getElementById("tabnamespan").innerHTML = "Teammates";
-        document.getElementById("tabbutton8").className = "coloredclickedbutton";
-    }
-
+	for (var i = 0; i < 9; i ++){
+		buttonname = "tabbutton" + i.toString();
+		document.getElementById(buttonname).className = "coloredbutton";
+		
+	}
+	
+	buttonname = "tabbutton" + tab.toString();
+	tabnum = tabnums[tab];	
+    document.getElementById("tabnamespan").innerHTML = tabnames[tab];	
+    document.getElementById(buttonname).className = "coloredclickedbutton";
+	
     setData(mon);
 }
 
@@ -224,8 +184,7 @@ function resetData(number, resetsearch) {
     document.getElementById("buttonlist").innerHTML = "";
 
     var value = select.options[select.selectedIndex].value;
-
-	setTotalUsage(value);
+	updateFormatInfo(value);
     mon = number;
     setTab(0);
     createButtons();
@@ -242,23 +201,16 @@ function updateSearch(){
 		pokename = dict[items[i][0]];
 		pokeid = items[i][0];
 		
-		if (pokename == null){			
-		}else{		
+		if (pokename != null){		
 			valueLowerCase = value.toLowerCase();
 			pokenameLowerCase = pokename.toLowerCase();
 		}
 		
 		var include = false;
 		
-		if (pokenameLowerCase.includes(valueLowerCase)){
+		if (pokenameLowerCase.includes(valueLowerCase) || pokeid.includes(value) || (i+1).toString().includes(value)){
 			include = true;
 		}
-		if (pokeid.includes(value)){
-			include = true;
-		}
-		if ((i+1).toString().includes(value)){
-			include = true;
-		}		
 
 		for (var alias in aliases){
 			if (alias.includes(value) && pokeid == aliases[alias]){
@@ -267,50 +219,24 @@ function updateSearch(){
 		}			
 		
 		if (include == false){
-			document.getElementById("button" + i.toString()).style.display = "none";			
-		}
-		if (include == true && document.getElementById("button" + i.toString()) != null){
+			try {				
+				document.getElementById("button" + i.toString()).style.display = "none";
+			} catch (e)	{
+				
+			}
+		} else if (include == true && document.getElementById("button" + i.toString()) != null){
 			document.getElementById("button" + i.toString()).style.display = "block";			
 		}
-			
 		
 	}
-	
-	
+		
 }
 
-function setTotalUsage(value){
-	var formatinfo = "";
-	if (value == "competition4101") {
-        items = competition4101;
-        totalusage = competition4101_totalusage;
-		formatinfo = "Battle of Alola - Junior Division - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/competitions/4101/\"> info about this competition </a>";
-    }
-    if (value == "competition4102") {
-        items = competition4102;
-        totalusage = competition4102_totalusage;
-		formatinfo = "Battle of Alola - Masters Division - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/competitions/4102/\"> info about this competition </a>";
-    }
-    if (value == "oras_117_1") {
-        items = oras_117_1;
-        totalusage = oras_117_1_totalusage;
-		formatinfo = "ORAS Season 17 - Singles";
-    }
-    if (value == "oras_117_2") {
-        items = oras_117_2;
-        totalusage = oras_117_2_totalusage;
-		formatinfo = "ORAS Season 17 - Doubles";
-    }
-    if (value == "oras_117_3") {
-        items = oras_117_3;
-        totalusage = oras_117_3_totalusage;
-		formatinfo = "ORAS Season 17 - Triples";
-    }
-    if (value == "oras_117_4") {
-        items = oras_117_4;
-        totalusage = oras_117_4_totalusage;
-		formatinfo = "ORAS Season 17 - Rotations";
-    }
+function updateFormatInfo(format){
+	var formatinfo = "";		
+    items = window[format];
+	totalusage = window[format + "_totalusage"];
+	formatinfo = formatinfodict[format];	
 	document.getElementById("formatinfo").innerHTML = "Selected format: " + formatinfo;
 }
 
