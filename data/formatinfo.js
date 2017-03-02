fa72bb3012a090d058d8c9cb158a20e811ec8c50
Boehijt02 <boehijt@gmail.com>
2017-03-02 23:31:18 +0100

    stuff

--- data/formatinfo.js
+++ data/formatinfo.js
@@ -1,6 +1,8 @@
 var formatinfodict = {
 	"competition4101": "Battle of Alola - Junior Division - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/competitions/4101/\"> info about this competition </a>",
 	"competition4102": "Battle of Alola - Masters Division - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/competitions/4102/\"> info about this competition </a>",
+	"competition4103": "Alola Friendly - Junior Division - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/competitions/4103/\"> info about this competition </a>",
+	"competition4104": "Alola Friendly - Masters Division - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/competitions/4104/\"> info about this competition </a>",
 	"oras_117_0": "ORAS Season 17 - Singles - NOTE: displaying ORAS data might cause errors or might have Pokemon missing in the list!",
 	"oras_117_1": "ORAS Season 17 - Singles - NOTE: displaying ORAS data might cause errors or might have Pokemon missing in the list!",
 	"oras_117_2": "ORAS Season 17 - Doubles - NOTE: displaying ORAS data might cause errors or might have Pokemon missing in the list!",
@@ -11,12 +13,6 @@ var formatinfodict = {
 	"sm_202_2": "SM Season 2 - Doubles - NOTE: these stats are a combination of post- and pre-bank stats!",
 	"sm_202_5": "SM Season 2 - Special - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/battle/202/regulation/\"> info about this season's special ladder</a> - NOTE: these stats are a combination of post- and pre-bank stats!",
 	"sm_202_6": "SM Season 2 - Championships (VGC/WCS) - NOTE: these stats are a combination of post- and pre-bank stats!",
-	"ps_gen7battlespotsingles_0_12_16": "PS! Gen 7 BSS stats for December 2016 and rating 0",
-	"ps_gen7battlespotsingles_1500_12_16": "PS! Gen 7 BSS stats for December 2016 and rating 1500",
 	"ps_gen7battlespotsingles_1630_12_16": "PS! Gen 7 BSS stats for December 2016 and rating 1630",
-	"ps_gen7battlespotsingles_1760_12_16": "PS! Gen 7 BSS stats for December 2016 and rating 1760",
-	"ps_gen7battlespotsingles_0_1_17": "PS! Gen 7 BSS stats for January 2016 and rating 0 - NOTE: these stats are a combination of post- and pre-bank stats!",
-	"ps_gen7battlespotsingles_1500_1_17": "PS! Gen 7 BSS stats for January 2016 and rating 1500 - NOTE: these stats are a combination of post- and pre-bank stats!",
-	"ps_gen7battlespotsingles_1630_1_17": "PS! Gen 7 BSS stats for January 2016 and rating 1630 - NOTE: these stats are a combination of post- and pre-bank stats!",
-	"ps_gen7battlespotsingles_1760_1_17": "PS! Gen 7 BSS stats for January 2016 and rating 1760 - NOTE: these stats are a combination of post- and pre-bank stats!"
+	"ps_gen7battlespotsingles_1630_1_17": "PS! Gen 7 BSS stats for January 2016 and rating 1630 - NOTE: these stats are a combination of post- and pre-bank stats!"
 }
