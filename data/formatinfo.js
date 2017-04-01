5fcd933b9fff0a93720078b124c41aa17fa08fb0
Boehijt02 <boehijt@gmail.com>
2017-04-01 15:57:37 +0200

    Update

--- data/formatinfo.js
+++ data/formatinfo.js
@@ -13,7 +13,12 @@ var formatinfodict = {
 	"sm_202_2": "SM Season 2 - Doubles - NOTE: these stats are a combination of post- and pre-bank stats!",
 	"sm_202_5": "SM Season 2 - Special - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/battle/202/regulation/\"> info about this season's special ladder</a> - NOTE: these stats are a combination of post- and pre-bank stats!",
 	"sm_202_6": "SM Season 2 - Championships (VGC/WCS)",
+	"sm_203_0": "SM Season 3 - All",
+	"sm_203_1": "SM Season 3 - Singles",
+	"sm_203_2": "SM Season 3 - Doubles",
+	"sm_203_5": "SM Season 3 - Special - <a target=\"blank\" href=\"https://3ds.pokemon-gl.com/battle/203/regulation/\"> info about this season's special ladder</a>",
+	"sm_203_6": "SM Season 3 - Championships (VGC/WCS)",
 	"ps_gen7battlespotsingles_1630_12_16": "PS! Gen 7 BSS stats for December 2016 and rating 1630 - NOTE: these are pre-bank stats",
 	"ps_gen7battlespotsingles_1630_1_17": "PS! Gen 7 BSS stats for January 2017 and rating 1630 - NOTE: these stats are a combination of post- and pre-bank stats!",
-	"ps_gen7battlespotsingles_1630_2_17": "PS! Gen 7 BSS stats for February 2017 and rating 1630"
+	"ps_gen7battlespotsingles_1630_1_17": "PS! Gen 7 BSS stats for February 2017 and rating 1630"
 }
